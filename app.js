b9c143cacff5b30d0fbaeb22b610d1117f2d62a5
Kevin Mitchell <loudwinston@gmail.com>
2014-08-01 00:02:00 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+
+var http = require('http');
+
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+
+
+
+
+app.get('*', function(req,res) {
+
+	var str = JSON.stringify(process.env, null, 2);
+	res.write("<html><body><pre>"+str+"</pre></body></html>");
+	res.end();
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
